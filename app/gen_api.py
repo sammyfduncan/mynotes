e323e3c5a04a1dbc8e5480d9d7e567dc8b72cfd8
sammyfduncan <SD192837465@hotmail.co.uk>
2025-08-05 20:27:56 +0100

    Added LLM integration and helpers

--- app/gen_api.py
+++ app/gen_api.py
@@ -1,1 +1,29 @@
-#util for llm API
+import os
+from google import generativeai
+from dotenv import load_dotenv
+#utils for llm API
+
+load_dotenv()
+
+def gen_notes(file_path : str) -> str:
+    #links api key in .env
+    generativeai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+    
+    #uploads file to google server
+    uploaded_file = generativeai.upload_file(path=file_path)
+
+    #create instance of model 
+    model = generativeai.GenerativeModel('gemini-2.0-flash')
+
+    #generate notes 
+    prompt = "Act as an academic assistant - create informative, well-explained but concise study notes based on this lecture."
+    try:
+        response = model.generate_content([prompt, uploaded_file])
+        return response.text
+    except Exception as e:
+        print(f"Error generating {e}")
+        return ""
+
+    
+
+
--- app/utils.py
+++ app/utils.py
@@ -13,46 +13,28 @@ def process_content(content_id : int):
         if not record:
             return
 
-        #extract text
-        if record.file_path.endswith(".pdf"):
-            extracted_text = extract_from_pdf(record.file_path)
-        elif record.file_path.endswith(".pptx"):
-            extracted_text = extract_from_pptx(record.file_path)
-        else:
-            record.status = "failed"
-            return
+        #now call llm util function
 
-        #call llm api function
 
+
+        #helper to save notes file 
+        def save_notes(
+                notes_content : str,
+                content_id : int
+        ) -> str:
+            note_path = f"notes/notes_{content_id}.md"
+            #open in write mode 
+            with open(note_path, "w", encoding="utf-8") as f:
+                f.write(notes_content)
+                return note_path
+            
+            
 
         #update record in memory
-        record.notes = extracted_text
+        record.file_path = note_path
         record.status = "complete"
 
         #commit changes to db
         db.commit()
 
-     
-def extract_from_pdf(file_path : str):
-    tmp = " "
-    
-    with open(file_path, "rb") as tmp_file:
-        reader = PyPDF2.PdfReader(tmp_file)
-        
-        for page in reader.pages:
-            tmp += page.extract_text()
-        return tmp
-    
-def extract_from_pptx(file_path : str):
-    tmp = " "
-
-    prs = Presentation(file_path)
-
-    for slide in prs.slides:
-        for shape in slide.shapes:
-            if not shape.has_text_frame:
-                continue
-            for paragraph in shape.text_frame.paragraphs:
-                for run in paragraph.runs:
-                    tmp += run.text
-    return tmp
+ 
