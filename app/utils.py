57bdf0172308daa11161c84ada24f72234408bb7
sammyfduncan <SD192837465@hotmail.co.uk>
2025-08-05 20:40:40 +0100

    Implemented file processing logic

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,7 @@ import PyPDF2
 from pptx import Presentation
 from .database import SessionLocal
 from .models import Content
+from .gen_api import gen_notes
 #backend logic 
 
 #worker function to process file contents 
@@ -14,27 +15,33 @@ def process_content(content_id : int):
             return
 
         #now call llm util function
+        note_str = gen_notes(file_path=record.file_path)
+        if not note_str:
+            record.status = "failed"
+            return
 
-
-
-        #helper to save notes file 
-        def save_notes(
-                notes_content : str,
-                content_id : int
-        ) -> str:
-            note_path = f"notes/notes_{content_id}.md"
-            #open in write mode 
-            with open(note_path, "w", encoding="utf-8") as f:
-                f.write(notes_content)
-                return note_path
-            
-            
+        #call helper
+        note_path = save_notes(
+            notes_content=note_str,
+            content_id=record.id
+        )
 
         #update record in memory
-        record.file_path = note_path
+        record.note_file_path = note_path
         record.status = "complete"
 
         #commit changes to db
         db.commit()
 
- 
+ #helper to save notes file 
+def save_notes(
+        notes_content : str,
+        content_id : int
+) -> str:
+    note_path = f"notes/notes_{content_id}.md"
+    #open in write mode 
+    with open(note_path, "w", encoding="utf-8") as f:
+        f.write(notes_content)
+        return note_path
+    
+    
